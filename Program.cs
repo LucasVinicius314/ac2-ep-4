2a5f2167926ba6b3fb3af43336986d72a6f4d738
Lucas Coelho <58010418+LucasVinicius314@users.noreply.github.com>
2022-04-15 02:47:56 -0300

    initial

---
+++ Program.cs
@@ -0,0 +1,126 @@
+namespace Ac2Ep4
+{
+  public static class Program
+  {
+    static string fileInPath = "assets/main.ula";
+    static string fileOutPath = "assets/main.hex";
+
+    static Dictionary<string, string> instructionDict = new Dictionary<string, string>
+    {
+      ["An"] = "0",
+      ["nAoB"] = "1",
+      ["AnB"] = "2",
+      ["zeroL"] = "3",
+      ["nAeB"] = "4",
+      ["Bn"] = "5",
+      ["AxB"] = "6",
+      ["ABn"] = "7",
+      ["AnoB"] = "8",
+      ["nAxB"] = "9",
+      ["copiaB"] = "A",
+      ["AB"] = "B",
+      ["umL"] = "C",
+      ["AoBn"] = "D",
+      ["AoB"] = "E",
+      ["copiaA"] = "F",
+    };
+
+    static void Main(string[] args)
+    {
+      var parsedInstructions = ParseInstructions();
+
+      var computedInstructions = ComputeInstructions(parsedInstructions);
+
+      WriteInstructions(computedInstructions);
+    }
+
+    static List<string> ParseInstructions()
+    {
+      using (var stream = new StreamReader(fileInPath))
+      {
+        var instructions = new List<string>();
+
+        while (true)
+        {
+          var res = stream.ReadLine();
+
+          if (res == null)
+          {
+            break;
+          }
+
+          instructions.Add(res);
+
+          if (stream.EndOfStream)
+          {
+            break;
+          }
+        }
+
+        instructions.RemoveAt(instructions.Count - 1);
+        instructions.RemoveAt(0);
+
+        stream.Dispose();
+
+        return instructions;
+      }
+    }
+
+    static List<string> ComputeInstructions(List<string> instructions)
+    {
+      var x = 0;
+      var y = 0;
+
+      var computedInstructions = new List<string>();
+
+      instructions.ForEach((instruction) =>
+      {
+        var type = instruction.Substring(0, 1);
+
+        var value = instruction.Substring(2, instruction.Length - 3);
+
+        if (type == "W")
+        {
+          var inst = instructionDict[value];
+
+          var hexX = x.ToString("X");
+          var hexY = y.ToString("X");
+
+          computedInstructions.Add($"{hexX}{hexY}{inst}");
+
+          return;
+        }
+
+        var parsedValue = int.Parse(value, System.Globalization.NumberStyles.HexNumber);
+
+        switch (type)
+        {
+          case "X":
+            x = parsedValue;
+            break;
+
+          case "Y":
+            y = parsedValue;
+            break;
+        }
+      });
+
+      return computedInstructions;
+    }
+
+    static void WriteInstructions(List<string> instructions)
+    {
+      using (var stream = new StreamWriter(fileOutPath, new FileStreamOptions
+      {
+        Mode = FileMode.Create,
+        Access = FileAccess.ReadWrite,
+        Share = FileShare.None,
+      }))
+      {
+        instructions.ForEach((inst) => stream.WriteLine(inst));
+
+        stream.Dispose();
+      }
+    }
+  }
+}
